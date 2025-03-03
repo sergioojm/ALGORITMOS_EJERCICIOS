c0771da52035685562e36abe13da95b91e13787c
Vlaskovich <sjulbez@gmail.com>
2025-03-03 20:11:11 +0100

    voraz

---
+++ practica_5/VorazCambio.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main(void)
+{
+
+    int monedas[8] = { 500, 200, 100, 50, 25, 10, 5, 1 };
+    int cantidad[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+
+
+    int dinero = 0;
+
+    cin >> dinero;
+
+    
+
+
+    return 0;
+}
